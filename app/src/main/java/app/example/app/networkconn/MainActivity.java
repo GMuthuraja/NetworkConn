649a72bd25c3957b49aeeddbbd5b235862eaaa89
Muthuraja <muthuraja.g@newgendigital.com>
2018-10-19 13:39:57 +0530

    Network Code Implementation

--- app/src/main/java/app/example/app/networkconn/MainActivity.java
+++ app/src/main/java/app/example/app/networkconn/MainActivity.java
@@ -1,7 +1,13 @@
 package app.example.app.networkconn;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void isNetworkAvailable(View v){
+        ConnectivityManager conn = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = conn.getActiveNetworkInfo();
+        if(info != null){
+            Toast.makeText(getApplicationContext(), info.getExtraInfo()+"", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(getApplicationContext(), "No Network found!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
